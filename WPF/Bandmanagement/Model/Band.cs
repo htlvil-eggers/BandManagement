67c27490de7070eb9ecb87969c44b3029940e61a
htlvil-delfserm <mikael.d@gmx.at>
2015-10-22 10:19:03 +0200

    StatusberichtThisWeek

---
+++ WPF/Bandmanagement/Model/Band.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandmanagement.Model
+{
+    public class Band
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+    }
+}
---
+++ WPF/Bandmanagement/Model/Musician.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandmanagement.Model
+{
+    public class Musician
+    {
+        public int Id { get; set; }
+
+        public String Username { get; set; }
+
+        public String Password { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public DateTime Birtdate { get; set; }
+
+        //public int HabitationId { get; set; }
+    }
+}
