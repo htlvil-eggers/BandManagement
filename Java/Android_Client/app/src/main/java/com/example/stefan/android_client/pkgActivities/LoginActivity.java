5e48602a64f6a57620509e7d83e1c1ee0c2e1c6c
htlvil-eggers <ibafluss@gmail.com>
2016-03-18 17:05:20 +0100

    Android app updated

--- Java/Android_Client/app/src/main/java/com/example/stefan/android_client/pkgActivities/LoginActivity.java
+++ Java/Android_Client/app/src/main/java/com/example/stefan/android_client/pkgActivities/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.example.stefan.android_client.pkgActivities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.stefan.android_client.R;
+import com.example.stefan.android_client.pkgModel.WebserviceManager;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText txtUsername;
     private EditText txtPassword;
     private EditText txtBandName;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +25,57 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         setComponentReferences();
+        setEventHandlers();
+    }
+
+    private void setEventHandlers() {
+        btnLogin.setOnClickListener(this);
     }
 
     private void setComponentReferences() {
         txtUsername = (EditText) this.findViewById (R.id.txtUsername);
         txtPassword = (EditText) this.findViewById(R.id.txtPassword);
         txtBandName = (EditText) this.findViewById(R.id.txtBandName);
+
+        btnLogin = (Button) this.findViewById(R.id.btnLogin);
     }
 
     private void btnLogin_onClick() {
-        String username = txtUsername.getText().toString();
-        String password = txtPassword.getText().toString();
-        String bandName = txtBandName.getText().toString();
+        final String username = txtUsername.getText().toString();
+        final String password = txtPassword.getText().toString();
+        final String bandName = txtBandName.getText().toString();
+        final Activity me = this;
 
-        if (/*checkCredentials*/ false) {
+        new AsyncTask<String,Void,Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... credentials) {
+                return WebserviceManager.checkCredentials(credentials[0], credentials[1], credentials[2]);
+            }
 
-        }
-        else {
-            Toast.makeText(this, R.string.credentialsNotCorrect, Toast.LENGTH_LONG);
+            protected void onPostExecute(Boolean credentialsCorrect)
+            {
+                if (credentialsCorrect) {
+                    Intent intent = new Intent(me, MainActivity.class);
+
+                    intent.putExtra("username", username);
+                    intent.putExtra("password", password);
+                    intent.putExtra("bandName", bandName);
+
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(me, R.string.credentialsNotCorrect, Toast.LENGTH_LONG).show();
+                }
+            }
+        }.execute(username, password, bandName);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnLogin:
+                btnLogin_onClick();
+                break;
         }
     }
 }
